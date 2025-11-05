1a3c91950ad381a2143bbcd2984149a5ccd5cd82
Branis24 <braniskaci78@gmail.com>
2025-11-05 11:35:43 +0100

    calendrier en contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,19 +1,50 @@
 "use client";
 
 import React from "react";
+import { Montserrat, Poppins } from "next/font/google";
 
-export default function ReservationPage() {
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["600", "700"], 
+  variable: "--font-montserrat",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400"], 
+  variable: "--font-poppins",
+});
+
+
+export default function ContactPage() {
   return (
     <div className="min-h-screen flex flex-col bg-[#D9C9B4]">
+          
       <main className="flex-grow flex flex-col items-center justify-center p-8 space-y-10">
-        <h1 className="text-3xl font-bold text-gray-800 text-center">
-          Réservation
+     
+        <h1
+          className="text-4xl text-gray-800 text-center tracking-wide"
+          style={{
+            fontFamily: "var(--font-montserrat)",
+            fontWeight: 700,
+          }}
+        >
+          Contact
         </h1>
 
-        <div
-          className="w-full max-w-4xl rounded-2xl shadow-lg overflow-hidden flex justify-center"
-          style={{ backgroundColor: "#f1f4f8" }}
-        >
+
+       <div className="w-full max-w-4xl rounded-2xl shadow-lg overflow-hidden flex flex-col items-center space-y-4 p-6"
+          style={{ backgroundColor: "#f1f4f8" }}>
+           <h2
+            className="text-2xl text-gray-800 text-center"
+            style={{
+              fontFamily: "var(--font-montserrat)",
+              fontWeight: 600,
+            }}
+          >
+            Horaire d’ouverture
+          </h2>
           <iframe
             src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&showPrint=0&mode=WEEK&showTabs=0&src=cmVuZGV6dm91cy5zeW5lZ29AZ21haWwuY29t&color=%23039be5"
             style={{ borderWidth: 0 }}
@@ -28,7 +59,13 @@ export default function ReservationPage() {
           className="w-full max-w-4xl rounded-2xl shadow-lg overflow-hidden flex flex-col items-center space-y-4 p-6"
           style={{ backgroundColor: "#f1f4f8" }}
         >
-          <h2 className="text-2xl font-semibold text-gray-800 text-center">
+         <h2
+            className="text-2xl text-gray-800 text-center"
+            style={{
+              fontFamily: "var(--font-montserrat)",
+              fontWeight: 600,
+            }}
+          >
             Localisation du cabinet
           </h2>
           <p className="text-gray-700 text-center">
